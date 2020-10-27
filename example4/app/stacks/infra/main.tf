3e799eefbb5a83c7af49962b86707697a0f8a18c
Tung Nguyen <tongueroo@gmail.com>
2020-10-27 05:23:38 -0700

    Update main.tf

--- example4/app/stacks/infra/main.tf
+++ example4/app/stacks/infra/main.tf
@@ -15,7 +15,7 @@
 stack = File.expand_path(".", __dir__)
 expr = "#{stack}/envs/#{Terraspace.env}/**.tf"
 Dir.glob(expr).each do |path|
-  mod = Terraspace::Mod.new("infra")
+  mod = Terraspace::Mod.new(@options[:mod]) # @options[:mod] = "infra"
   puts Terraspace::Compiler::Erb.new(mod, path).build
 end
 %>
